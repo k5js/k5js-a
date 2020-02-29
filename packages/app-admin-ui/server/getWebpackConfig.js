4740fc8691717214c4b9901e068df9c9e582b389
Gautam Singh <gautamsi@users.noreply.github.com>
2020-03-01 00:44:58 +0530

    fix package loading

--- packages/app-admin-ui/server/getWebpackConfig.js
+++ packages/app-admin-ui/server/getWebpackConfig.js
@@ -58,7 +58,7 @@ module.exports = function({ adminMeta, entry, outputPath }) {
       test: /FIELD_TYPES/,
       use: [
         {
-          loader: '@keystonejs/field-views-loader',
+          loader: '@k5js/field-views-loader',
           options: {
             adminMeta,
           },
